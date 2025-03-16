7e50429e27d3c5a8bd144dab86ea3a329b74fb57
DasithKarunarathne <dasithkarunarathne18@gmail.com>
2025-03-16 22:44:01 +0530

    pettycash
    frontend

--- frontend/src/components/AddPettyCash.jsx
+++ frontend/src/components/AddPettyCash.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-
+import { Box, Button, TextField, MenuItem, Select } from "@mui/material";
 import axios from "axios";
 
 const AddTransaction = ({fetchTransactions}) =>{
@@ -22,13 +22,13 @@ const AddTransaction = ({fetchTransactions}) =>{
                 category,
             });
 
-            if(response.status===201){
+            if (response.status === 201) {
                 fetchTransactions();
-                setdescription=("");
-                setAmount=("");
-                settype=("expense");
-                setCategory=("");
-            }
+                setdescription("");
+                setAmount("");
+                settype("expense");
+                setCategory("");
+              }
             
         } catch (error) {
             const msg = error.response?.data?.message || "An unexpeced error occured";
@@ -40,6 +40,74 @@ const AddTransaction = ({fetchTransactions}) =>{
 
     const closeMdodal  =() => setErrormsg("");
     
+    return(
+
+        <Box sx={{
+            p: 4,
+            maxWidth: "400px", // Similar to max-w-md
+            mx: "auto", // Center horizontally
+          }}
+        >
+            <form onSubmit={handleSubmit}> 
+
+            <TextField
+          label="Description"
+          variant="outlined"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+          fullWidth
+          sx={{ mb: 2 }} // Margin bottom for spacing
+        />
+
+        {/* Amount Input */}
+        <TextField
+          label="Amount"
+          type="number"
+          variant="outlined"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+          fullWidth
+          sx={{ mb: 2 }}
+        />
+
+<Select
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+          fullWidth
+          variant="outlined"
+          displayEmpty
+          sx={{ mb: 2 }}
+        >
+
+<MenuItem value="" disabled>
+            Select type
+          </MenuItem>
+          <MenuItem value="expense">Expense</MenuItem>
+          <MenuItem value="reimbursement">Reimbursement</MenuItem>
+          <MenuItem value="initial">Initial</MenuItem>
+        </Select>
+
+
+        <TextField
+          label="Category"
+          variant="outlined"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+          fullWidth
+          sx={{ mb: 2 }}
+        />
+
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+            Add Transaction
+        </Button>
+            </form>
+            </Box>
+    );
     
 
-}
+};
+
+export default AddTransaction;
