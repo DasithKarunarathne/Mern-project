1c92c27b058a405f23c06ea3d98fa78cf2a9e252
it23377844 <it23377844@my.sliit.lk>
2025-03-25 09:31:37 +0530

    added inventory
    inventory

--- backend/controllers/feedback.controller.js
+++
@@ -1,8 +0,0 @@
-/*import mongoose from "mongoose";
-
-import feedback from "../models/feedback";
-
-export const editFeedback = (req, res) => {
-    Feedback.findByIdAndUpdate(req.params.id, req.body)
-      .then(() => res.json({ msg: "Update successful" }))
-      .catch(() => res.status(400).json({ msg: "Update failed" }));*/
